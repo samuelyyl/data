630c061d58c810f37ce1db984bc75992f1ef41b6
yeyunlong <yeyunlong@ghzhushou.com>
2025-09-10 17:24:47 +0800

    add csv parse & update data

--- kg/webpack.config.js
+++ kg/webpack.config.js
@@ -4,20 +4,28 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js', // 入口文件
     mode: 'development', // 设置开发模式
+    // mode: 'production', // 设置开发模式
     output: {
         path: path.resolve(__dirname, 'js'), // 输出目录
         // path: __dirname+'/js', // 输出目录
         filename: 'index.min.js' // 输出文件名
     },
+    devtool: false, // 不输出 source map
     plugins: [
         new JavaScriptObfuscator({
             compact: true,
-            controlFlowFlattening: true
+            controlFlowFlattening: true,
+            stringArray: true,
+            stringArrayEncoding: ['base64'] // JSON/CSV里的字符串会被编码
             // 在这里可以添加更多的混淆选项
         }, ['excluded_bundle_name.js'])
     ],
     module: {
         rules: [
+            {
+                test: /\.csv$/i,
+                use: ['csv-loader']
+            }
         ]
     }
 };
