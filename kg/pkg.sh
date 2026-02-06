289e94be2b09dbb39d9a4539fbe247a24f15159d
yeyunlong <yeyunlong@ghzhushou.com>
2026-02-06 14:19:32 +0800

    update data

--- kg/pkg.sh
+++ kg/pkg.sh
@@ -7,6 +7,7 @@ source .env
 ENCRYPT_PASSWORD=$ENCRYPT_PASSWORD npm run build
 
 # 从 line.dev.html 生成 line.html
-sed 's|<script type="module" src="./src/index.js"></script>|<script type="text/javascript" src="./js/index.min.js"></script>|' line.dev.html > line.html
+sed 's|<script type="module" src="./src/index.js"></script>|<script type="text/javascript" src="./js/index.min.js"></script>|' line.dev.html | \
+sed 's|<title>KG (Dev)</title>|<title>KG</title>|' > line.html
 
 echo "✅ Generated line.html from line.dev.html"
